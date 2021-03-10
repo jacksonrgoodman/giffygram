0bc5b1ec4d9059f0d7d3247699f1e6f98f5b49b5
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-10 09:59:47 -0600

    added images and event listeners, and a footer

---
+++ src/scripts/nav/Footer.js
@@ -0,0 +1,14 @@
+export const Footer = () => {
+    // HTML to be returned to GiffyGram component
+    return `
+        <div class="footer__item">
+            Posts since <select id="yearSelection">
+                <option>2020</option>
+                <option>2019</option>
+                <option>2018</option>
+                <option>2017</option>
+            </select>
+            <span id="postCount">0</span>
+        </div>
+    `
+}
---
+++ src/scripts/nav/NavBar.js
@@ -0,0 +1,21 @@
+export const NavBar = () => {
+    return `
+        <nav class="navigation">
+            <div class="navigation__item navigation__icon">
+                <img id="homeIcon" src="../src/scripts/images/pb.png" alt="Giffygram icon" />
+            </div>
+            <div class="navigation__item navigation__name">
+                Giffygram
+            </div>
+            <div class="navigation__item navigation__search">
+                <input type="text" id="postSearch" placeholder="Search posts..." />
+            </div>
+            <div class="navigation__item navigation__message">
+                <img id="directMessageIcon" src="../src/scripts/images/fountain-pen.svg" alt="Direct Message" />
+            </div>
+            <div class="navigation__item navigation__logout">
+                <button id="logout" class="fakeLink">Logout</button>
+            </div>
+        </nav>
+    `
+}
