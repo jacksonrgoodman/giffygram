99b77cd8be18ec557465557d180de8b2737a76db
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-10 11:29:53 -0600

    changed timestamps so the filtering makes more of an impact, changed filter method

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -3,11 +3,6 @@ export const getUsers = () => {
     .then(response => response.json())
 };
 
-export const getPosts = () => {
-    return fetch("http://localhost:8088/posts")
-    .then(response => response.json())
-};
-
 const loggedInUser = {
 	id: 1,
 	name: "Jackson",
@@ -17,3 +12,20 @@ const loggedInUser = {
 export const getLoggedInUser = () => {
 	return loggedInUser;
 };
+
+let postCollection = [];
+
+export const usePostCollection = () => {
+  //Best practice: we don't want to alter the original state, so
+  //make a copy of it and then return it
+  //The spread [...] operator makes this quick work
+  return [...postCollection];
+}
+export const getPosts = () => {
+  return fetch("http://localhost:8088/posts")
+    .then(response => response.json())
+    .then(parsedResponse => {
+      postCollection = parsedResponse
+      return parsedResponse;
+    })
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-import {getPosts} from "./data/DataManager.js";
+import {getPosts, usePostCollection} from "./data/DataManager.js";
 import {PostList} from "./feed/PostList.js";
 import {NavBar} from "./nav/NavBar.js";
 import {Footer} from "./nav/Footer.js";
@@ -47,6 +47,28 @@ applicationElement.addEventListener("change", event => {
       console.log(`User wants to see posts since ${yearAsNumber}`)
     }
 })
+
+applicationElement.addEventListener("change", event => {
+    if (event.target.id === "yearSelection") {
+      const yearAsNumber = parseInt(event.target.value)
+      console.log(`User wants to see posts since ${yearAsNumber}`)
+      //invoke a filter function passing the year as an argument
+      showFilteredPosts(yearAsNumber);
+    }
+  })
+  
+  const showFilteredPosts = (year) => {
+    //get a copy of the post collection
+    const epoch = Date.parse(`01/01/${year}`);
+    //filter the data
+    const filteredData = usePostCollection().filter(singlePost => {
+      if (singlePost.timestamp >= epoch) {
+        return singlePost
+      }
+    })
+    const postElement = document.querySelector(".postList");
+    postElement.innerHTML = PostList(filteredData);
+}
 
 const showPostList = () => {
     const postElement = document.querySelector(".postList");
